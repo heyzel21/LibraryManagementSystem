939b84e7d74d8be47d58171f0cdc796ad696a6b4
heyzel21 <hcabahug3@gmail.com>
2024-12-10 02:38:42 +0800

    count for list table

--- LibraryManagementSystem/DashboardPanel.Designer.cs
+++ LibraryManagementSystem/DashboardPanel.Designer.cs
@@ -45,12 +45,16 @@ namespace LibraryManagementSystem
             this.barUsersPanel = new System.Windows.Forms.Panel();
             this.data1Panel = new System.Windows.Forms.Panel();
             this.numUsersPenaltyPanel = new System.Windows.Forms.Panel();
-            this.numUsersPenaltyLbl = new System.Windows.Forms.Label();
+            this.noOfstudentPenaltyPlaceholder = new System.Windows.Forms.Label();
+            this.noOfStudentsPenaltyLbl = new System.Windows.Forms.Label();
             this.numBorrowBookPanel = new System.Windows.Forms.Panel();
-            this.numBorrowedBooksLbl = new System.Windows.Forms.Label();
+            this.numberOfBooksPlaceholder = new System.Windows.Forms.Label();
+            this.numBooksLbl = new System.Windows.Forms.Label();
             this.numReturnBookPanel = new System.Windows.Forms.Panel();
+            this.issuedBooksPlaceholder = new System.Windows.Forms.Label();
             this.numReturneedBooksLbl = new System.Windows.Forms.Label();
             this.numUsersPanel = new System.Windows.Forms.Panel();
+            this.noOfStudentsPlaceholder = new System.Windows.Forms.Label();
             this.numUsersLbl = new System.Windows.Forms.Label();
             this.data2Panel.SuspendLayout();
             this.data1Panel.SuspendLayout();
@@ -197,82 +201,126 @@ namespace LibraryManagementSystem
             // numUsersPenaltyPanel
             // 
             this.numUsersPenaltyPanel.BackColor = System.Drawing.Color.SkyBlue;
-            this.numUsersPenaltyPanel.Controls.Add(this.numUsersPenaltyLbl);
+            this.numUsersPenaltyPanel.Controls.Add(this.noOfstudentPenaltyPlaceholder);
+            this.numUsersPenaltyPanel.Controls.Add(this.noOfStudentsPenaltyLbl);
             this.numUsersPenaltyPanel.Location = new System.Drawing.Point(547, 15);
             this.numUsersPenaltyPanel.Name = "numUsersPenaltyPanel";
             this.numUsersPenaltyPanel.Size = new System.Drawing.Size(164, 161);
             this.numUsersPenaltyPanel.TabIndex = 3;
             // 
-            // numUsersPenaltyLbl
+            // noOfstudentPenaltyPlaceholder
             // 
-            this.numUsersPenaltyLbl.AutoSize = true;
-            this.numUsersPenaltyLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numUsersPenaltyLbl.ForeColor = System.Drawing.Color.Black;
-            this.numUsersPenaltyLbl.Location = new System.Drawing.Point(21, 22);
-            this.numUsersPenaltyLbl.Name = "numUsersPenaltyLbl";
-            this.numUsersPenaltyLbl.Size = new System.Drawing.Size(119, 16);
-            this.numUsersPenaltyLbl.TabIndex = 0;
-            this.numUsersPenaltyLbl.Text = "USERS PENALTY";
+            this.noOfstudentPenaltyPlaceholder.AutoSize = true;
+            this.noOfstudentPenaltyPlaceholder.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noOfstudentPenaltyPlaceholder.Location = new System.Drawing.Point(55, 51);
+            this.noOfstudentPenaltyPlaceholder.Name = "noOfstudentPenaltyPlaceholder";
+            this.noOfstudentPenaltyPlaceholder.Size = new System.Drawing.Size(68, 73);
+            this.noOfstudentPenaltyPlaceholder.TabIndex = 4;
+            this.noOfstudentPenaltyPlaceholder.Text = "0";
+            // 
+            // noOfStudentsPenaltyLbl
+            // 
+            this.noOfStudentsPenaltyLbl.AutoSize = true;
+            this.noOfStudentsPenaltyLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noOfStudentsPenaltyLbl.ForeColor = System.Drawing.Color.Black;
+            this.noOfStudentsPenaltyLbl.Location = new System.Drawing.Point(12, 22);
+            this.noOfStudentsPenaltyLbl.Name = "noOfStudentsPenaltyLbl";
+            this.noOfStudentsPenaltyLbl.Size = new System.Drawing.Size(138, 16);
+            this.noOfStudentsPenaltyLbl.TabIndex = 0;
+            this.noOfStudentsPenaltyLbl.Text = "STUDENT PENALTY";
             // 
             // numBorrowBookPanel
             // 
             this.numBorrowBookPanel.BackColor = System.Drawing.Color.SkyBlue;
-            this.numBorrowBookPanel.Controls.Add(this.numBorrowedBooksLbl);
+            this.numBorrowBookPanel.Controls.Add(this.numberOfBooksPlaceholder);
+            this.numBorrowBookPanel.Controls.Add(this.numBooksLbl);
             this.numBorrowBookPanel.Location = new System.Drawing.Point(188, 15);
             this.numBorrowBookPanel.Name = "numBorrowBookPanel";
             this.numBorrowBookPanel.Size = new System.Drawing.Size(164, 161);
             this.numBorrowBookPanel.TabIndex = 1;
             // 
-            // numBorrowedBooksLbl
+            // numberOfBooksPlaceholder
             // 
-            this.numBorrowedBooksLbl.AutoSize = true;
-            this.numBorrowedBooksLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numBorrowedBooksLbl.ForeColor = System.Drawing.Color.Black;
-            this.numBorrowedBooksLbl.Location = new System.Drawing.Point(12, 22);
-            this.numBorrowedBooksLbl.Name = "numBorrowedBooksLbl";
-            this.numBorrowedBooksLbl.Size = new System.Drawing.Size(137, 16);
-            this.numBorrowedBooksLbl.TabIndex = 0;
-            this.numBorrowedBooksLbl.Text = "BORROWED BOOKS";
+            this.numberOfBooksPlaceholder.AutoSize = true;
+            this.numberOfBooksPlaceholder.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberOfBooksPlaceholder.Location = new System.Drawing.Point(45, 51);
+            this.numberOfBooksPlaceholder.Name = "numberOfBooksPlaceholder";
+            this.numberOfBooksPlaceholder.Size = new System.Drawing.Size(68, 73);
+            this.numberOfBooksPlaceholder.TabIndex = 2;
+            this.numberOfBooksPlaceholder.Text = "0";
+            // 
+            // numBooksLbl
+            // 
+            this.numBooksLbl.AutoSize = true;
+            this.numBooksLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numBooksLbl.ForeColor = System.Drawing.Color.Black;
+            this.numBooksLbl.Location = new System.Drawing.Point(28, 22);
+            this.numBooksLbl.Name = "numBooksLbl";
+            this.numBooksLbl.Size = new System.Drawing.Size(100, 16);
+            this.numBooksLbl.TabIndex = 0;
+            this.numBooksLbl.Text = "NO. OF BOOKS";
             // 
             // numReturnBookPanel
             // 
             this.numReturnBookPanel.BackColor = System.Drawing.Color.SkyBlue;
+            this.numReturnBookPanel.Controls.Add(this.issuedBooksPlaceholder);
             this.numReturnBookPanel.Controls.Add(this.numReturneedBooksLbl);
             this.numReturnBookPanel.Location = new System.Drawing.Point(368, 15);
             this.numReturnBookPanel.Name = "numReturnBookPanel";
             this.numReturnBookPanel.Size = new System.Drawing.Size(164, 161);
             this.numReturnBookPanel.TabIndex = 2;
             // 
+            // issuedBooksPlaceholder
+            // 
+            this.issuedBooksPlaceholder.AutoSize = true;
+            this.issuedBooksPlaceholder.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.issuedBooksPlaceholder.Location = new System.Drawing.Point(50, 51);
+            this.issuedBooksPlaceholder.Name = "issuedBooksPlaceholder";
+            this.issuedBooksPlaceholder.Size = new System.Drawing.Size(68, 73);
+            this.issuedBooksPlaceholder.TabIndex = 3;
+            this.issuedBooksPlaceholder.Text = "0";
+            // 
             // numReturneedBooksLbl
             // 
             this.numReturneedBooksLbl.AutoSize = true;
             this.numReturneedBooksLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.numReturneedBooksLbl.ForeColor = System.Drawing.Color.Black;
-            this.numReturneedBooksLbl.Location = new System.Drawing.Point(23, 22);
+            this.numReturneedBooksLbl.Location = new System.Drawing.Point(33, 22);
             this.numReturneedBooksLbl.Name = "numReturneedBooksLbl";
-            this.numReturneedBooksLbl.Size = new System.Drawing.Size(133, 16);
+            this.numReturneedBooksLbl.Size = new System.Drawing.Size(106, 16);
             this.numReturneedBooksLbl.TabIndex = 0;
-            this.numReturneedBooksLbl.Text = "RETURNED BOOKS";
+            this.numReturneedBooksLbl.Text = "ISSUED BOOKS";
             // 
             // numUsersPanel
             // 
             this.numUsersPanel.BackColor = System.Drawing.Color.SkyBlue;
+            this.numUsersPanel.Controls.Add(this.noOfStudentsPlaceholder);
             this.numUsersPanel.Controls.Add(this.numUsersLbl);
             this.numUsersPanel.Location = new System.Drawing.Point(9, 15);
             this.numUsersPanel.Name = "numUsersPanel";
             this.numUsersPanel.Size = new System.Drawing.Size(164, 161);
             this.numUsersPanel.TabIndex = 0;
             // 
+            // noOfStudentsPlaceholder
+            // 
+            this.noOfStudentsPlaceholder.AutoSize = true;
+            this.noOfStudentsPlaceholder.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noOfStudentsPlaceholder.Location = new System.Drawing.Point(50, 51);
+            this.noOfStudentsPlaceholder.Name = "noOfStudentsPlaceholder";
+            this.noOfStudentsPlaceholder.Size = new System.Drawing.Size(68, 73);
+            this.noOfStudentsPlaceholder.TabIndex = 1;
+            this.noOfStudentsPlaceholder.Text = "0";
+            // 
             // numUsersLbl
             // 
             this.numUsersLbl.AutoSize = true;
             this.numUsersLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.numUsersLbl.ForeColor = System.Drawing.Color.Black;
-            this.numUsersLbl.Location = new System.Drawing.Point(32, 22);
+            this.numUsersLbl.Location = new System.Drawing.Point(19, 22);
             this.numUsersLbl.Name = "numUsersLbl";
-            this.numUsersLbl.Size = new System.Drawing.Size(101, 16);
+            this.numUsersLbl.Size = new System.Drawing.Size(129, 16);
             this.numUsersLbl.TabIndex = 0;
-            this.numUsersLbl.Text = "NO. OF USERS";
+            this.numUsersLbl.Text = "NO. OF STUDENTS";
             // 
             // DashboardPanel
             // 
@@ -303,9 +351,9 @@ namespace LibraryManagementSystem
 
         private Panel data1Panel;
         private Panel numUsersPenaltyPanel;
-        private Label numUsersPenaltyLbl;
+        private Label noOfStudentsPenaltyLbl;
         private Panel numBorrowBookPanel;
-        private Label numBorrowedBooksLbl;
+        private Label numBooksLbl;
         private Panel numReturnBookPanel;
         private Label numReturneedBooksLbl;
         private Panel numUsersPanel;
@@ -323,5 +371,9 @@ namespace LibraryManagementSystem
         private Label legendUsersPenaltyLbl;
         private Label legendReturnedBooksLbl;
         private Label legendBorrowedBooksLbl;
+        private Label noOfStudentsPlaceholder;
+        private Label noOfstudentPenaltyPlaceholder;
+        private Label numberOfBooksPlaceholder;
+        private Label issuedBooksPlaceholder;
     }
 }
--- LibraryManagementSystem/DashboardPanel.cs
+++ LibraryManagementSystem/DashboardPanel.cs
@@ -12,9 +12,36 @@ namespace LibraryManagementSystem
 {
     public partial class DashboardPanel : UserControl
     {
+        private readonly string connectionString = "Server=localhost;Database=lms_db;User ID=root;Password=;";
+        private BookService bookService;
+        private StudentService studentService;
+        private IssueReturnBookService issueReturnBookService;
+
         public DashboardPanel()
         {
             InitializeComponent();
+
+            this.bookService = new BookService(connectionString);
+            this.studentService = new StudentService(connectionString);
+            this.issueReturnBookService = new IssueReturnBookService(connectionString);
+
+            this.PopulateData();
+        }
+
+        private void PopulateData()
+        {
+            try
+            {
+                this.noOfStudentsPlaceholder.Text = this.studentService.List().Count.ToString();
+                this.numberOfBooksPlaceholder.Text = this.bookService.List().Count.ToString();
+                this.issuedBooksPlaceholder.Text = this.issueReturnBookService.List().Count.ToString();
+                this.noOfstudentPenaltyPlaceholder.Text = this.issueReturnBookService.ListDue().Count.ToString();
+            }
+            catch (Exception ex) 
+            {
+                Console.Write(ex.ToString());
+            }
+
         }
     }
 }
